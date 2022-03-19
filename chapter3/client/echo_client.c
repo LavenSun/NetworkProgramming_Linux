edfee4d522f937c4f77c4539ef2a0171c2a20f7d
laven <570350003@qq.com>
2022-03-19 16:52:47 +0800

    update echo_client.c of chapter3

--- chapter3/client/echo_client.c
+++ chapter3/client/echo_client.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 
         write(sock, message, strlen(message));
         str_len = read(sock, message, BUF_SIZE - 1);
-        message[str_len] = 0;
+        message[str_len] = '\0';
         printf("Message from server: %s\n", message);
     }
 
